d28b1be35cc7daabc0a665cc862b325b5adb9430
ILya Shirogorov <ilyaplayrix@gmail.com>
2021-12-08 23:29:10 +0300

    fix screen_text_snip.bat

--- screen_text_snip.bat
+++ screen_text_snip.bat
@@ -1,7 +1,7 @@
 set this_dir=%~dp0
 
 pushd "%this_dir%"
-call ".venv\Scripts\activate.bat"
-python "screen_text_snip.py" %*
-call ".venv\Scripts\deactivate.bat"
+rem call ".venv\Scripts\activate.bat"
+python screen_text_snip.py %*
+rem call ".venv\Scripts\deactivate.bat"
 popd
